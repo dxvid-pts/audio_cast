9158c029b59d12a03a10e17b3f8142d2b5add9d6
David <8vsxdxd@gmail.com>
2021-01-27 09:22:08 +0100

    Migrate to dart package

---
+++ example/audio_cast_example.dart
@@ -0,0 +1,6 @@
+import 'package:audio_cast/audio_cast.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/audio_cast.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library audio_cast;
+
+export 'src/audio_cast_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
